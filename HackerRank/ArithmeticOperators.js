b3716cc706a32c37de836565b482bed25664ac50
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-04-22 10:17:46 -0400

    Arithmetic Operators

---
+++ HackerRank/ArithmeticOperators.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getArea(length, width) {
+    let area = length * width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = 2 * (length + width);
+    return perimeter;
+}
+
