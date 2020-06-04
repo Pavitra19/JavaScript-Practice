1635637204a3192f2a8a1fef95b32efead9f6e15
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-06-04 12:31:51 -0400

    Factorials

--- HackerRank/Functions.js
+++ HackerRank/Functions.js
@@ -1,35 +1,16 @@
-'use strict';
+// calculating factorial 
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-console.log('Input number');
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-
-    main();
-
-    console.log("Program ended");
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
+// method 1 
 const factorial = (n) => {
     return (n === 0 ? 1 : n * factorial(n - 1));
 }
 
-function main() {
-    const n = +(readLine());
-    console.log(factorial(n));
+// method 2
+function fact(n) {
+    var factorial = 1;
+    while (n > 0) {
+        factorial = n * factorial;
+        n--;
+    }
+    console.log(factorial);
 }
