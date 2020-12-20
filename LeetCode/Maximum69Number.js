42abd6f45768deb3a7a70704fb59a43b0f0e00c5
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-12-19 19:44:57 -0500

    Add second method to Maximum 69 Number

--- LeetCode/Maximum69Number.js
+++ LeetCode/Maximum69Number.js
@@ -7,6 +7,11 @@
  * Leetcode 1323
  */
 
+// Runtime: 64 ms (faster than 99.66%), Memory Usage: 38.9 MB (less than 6.59%)
+var maximum69Number = function (num) {
+  return num.toString().replace("6", "9"); // convert to string, then replace first occurance of 6
+};
+
 // Runtime: 80 ms (aster than 62.16%), Memory Usage: 38.7 MB (less than 44.26%)
 var maximum69Number = function (num) {
   let nums = [...num.toString()];
