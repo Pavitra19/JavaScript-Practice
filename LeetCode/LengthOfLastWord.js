0170233acbf2ac589412120ec422ba9af54fda0c
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-12-26 18:24:02 -0500

    Add third method to Length of Last Word

--- LeetCode/LengthOfLastWord.js
+++ LeetCode/LengthOfLastWord.js
@@ -7,14 +7,19 @@
  * Leetcode 58
  */
 
-// Runtime: 64 ms (faster than 99.72%), Memory Usage: 38.8 MB (less than 17.34%)
+// Runtime: 64 ms (faster than 99.72%), Memory Usage: 38.2 MB (less than 94.55%)
 var lengthOfLastWord = function (s) {
+  return s.trim().length - s.trim().lastIndexOf(" ") - 1; // subtract first and last index of the last word
+};
+
+// Runtime: 64 ms (faster than 99.72%), Memory Usage: 38.8 MB (less than 17.34%)
+var lengthOfLastWord2 = function (s) {
   let lastWord = s.trim().split(" ").pop(); // trim whitespaces from both ends of string, split words, pop off last word
   return lastWord.length;
 };
 
 // Runtime: 72 ms (faster than 92.50%), Memory Usage: 38.6 MB (less than 60.30%)
-var lengthOfLastWord2 = function (s) {
+var lengthOfLastWord3 = function (s) {
   s = s.split(" ").filter((word) => word); // split words, filter out any spaces
   return s.length > 0 ? s[s.length - 1].length : 0; // if empty string, return 0, else return length of last word
 };
