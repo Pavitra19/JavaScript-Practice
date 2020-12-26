034e2ca85e052490c3bc5dfdd63f1da64a8ff824
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-12-26 17:19:57 -0500

    Add another method to XOR Operation In An Array

--- LeetCode/XOROperationInAnArray.js
+++ LeetCode/XOROperationInAnArray.js
@@ -8,8 +8,17 @@
  * Leetcode 1486
  */
 
-// Runtime: 80 ms (faster than 45.87%), Memory Usage: 38.8 MB (less than 14.26%)
+// Runtime: 76 ms (faster than 75.20%), Memory Usage: 38.2 MB (less than 92.06%)
 var xorOperation = function (n, start) {
+  let xor = start;
+  for (let i = 1; i < n; i++) {
+    xor = xor ^ (start + 2 * i); // Bitwise XOR returns 1 if only one of two bits is 1
+  }
+  return xor;
+};
+
+// Runtime: 80 ms (faster than 45.87%), Memory Usage: 38.8 MB (less than 14.26%)
+var xorOperation2 = function (n, start) {
   let nums = [];
   for (let i = 0; i < n; i++) {
     nums[i] = start + 2 * i;
