91d734beb5131687f0be08079dca4578beee93e1
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-12-29 19:04:37 -0500

    Add second method to Range Sum of BST

--- LeetCode/RangeSumOfBST.js
+++ LeetCode/RangeSumOfBST.js
@@ -15,9 +15,25 @@
  * Leetcode 938
  */
 
-// Runtime: 208 ms (faster than 99.01%), Memory Usage: 68.6 MB (less than 11.10%)
+// Runtime: 232 ms (faster than 57.09%), Memory Usage: 68 MB (less than 59.18%)
 // Time: O(n), space: O(n)
 var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+  var checkValue = function (node) {
+    if (!node) return;
+    if (node.val >= low && node.val <= high) {
+      sum += node.val;
+    }
+    if (low < node.val) checkValue(node.left); // left branch has values inside the range
+    if (node.val < high) checkValue(node.right); // right branch has values inside the range
+  };
+  checkValue(root);
+  return sum;
+};
+
+// Runtime: 208 ms (faster than 99.01%), Memory Usage: 68.6 MB (less than 11.10%)
+// Time: O(n), space: O(n)
+var rangeSumBST2 = function (root, low, high) {
   let sum = 0;
   // function to traverse through nodes
   var checkValue = function (node) {
