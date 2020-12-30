b96cb0799d29687a50791fa93b4bf59d1fd8a174
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-12-29 19:13:54 -0500

    Add third  method to Range Sum of BST

--- LeetCode/RangeSumOfBST.js
+++ LeetCode/RangeSumOfBST.js
@@ -14,10 +14,26 @@
  * Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
  * Leetcode 938
  */
+// Runtime: 228 ms (faster than 69.62%), Memory Usage: 68.2 MB (less than 52.31%)
+// Time: O(n), space: O(n)
+var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+  if (!root) return sum;
+  if (root.val >= low && root.val <= high) {
+    sum += root.val;
+  }
+  if (low < root.val) {
+    sum += rangeSumBST(root.left, low, high);
+  }
+  if (root.val < high) {
+    sum += rangeSumBST(root.right, low, high);
+  }
+  return sum;
+};
 
 // Runtime: 232 ms (faster than 57.09%), Memory Usage: 68 MB (less than 59.18%)
 // Time: O(n), space: O(n)
-var rangeSumBST = function (root, low, high) {
+var rangeSumBST2 = function (root, low, high) {
   let sum = 0;
   var checkValue = function (node) {
     if (!node) return;
@@ -33,7 +49,7 @@ var rangeSumBST = function (root, low, high) {
 
 // Runtime: 208 ms (faster than 99.01%), Memory Usage: 68.6 MB (less than 11.10%)
 // Time: O(n), space: O(n)
-var rangeSumBST2 = function (root, low, high) {
+var rangeSumBST3 = function (root, low, high) {
   let sum = 0;
   // function to traverse through nodes
   var checkValue = function (node) {
