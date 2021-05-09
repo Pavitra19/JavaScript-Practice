fb07074be5f17b837dee327796c42d7388bf8ba3
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-05-09 15:17:45 -0400

    Add another method to valid palindrome

--- LeetCode/Easy/ValidPalindrome.js
+++ LeetCode/Easy/ValidPalindrome.js
@@ -25,15 +25,15 @@ var isPalindrome2 = function (s) {
   return true;
 };
 
-// Runtime: 92 ms, faster than 81.09% of JavaScript online submissions for Valid Palindrome.
-// Memory Usage: 40.9 MB, less than 88.76 % of JavaScript online submissions
+// Runtime: 88 ms, faster than 91.01% of JavaScript online submissions for Valid Palindrome.
+// Memory Usage: 41.1 MB, less than 74.48% of JavaScript online submissions
 var isPalindrome3 = function (s) {
   s = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   let i = Math.floor(s.length / 2); // initialize pointer in middle
   let j = s.length % 2 === 0 ? i - 1 : i; // if string is even, j pointer will be behind right pointer, so that left/right pointers are pointing at the 2 middle values
 
   // loop through the string while moving pointers outwards comparing the characters
-  while (j < s.length) {
+  while (i >= 0 && j < s.length) {
     if (s[i] !== s[j]) {
       return false;
     }
@@ -43,6 +43,21 @@ var isPalindrome3 = function (s) {
   }
 
   return true;
+};
+
+// Runtime: 92 ms, faster than 81.09% of JavaScript online submissions for Valid Palindrome.
+// Memory Usage: 43.1 MB, less than 47.73% of JavaScript online submissions for Valid Palindrome.
+var isPalindrome4 = function (s) {
+  s = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  let sReversed = "";
+
+  // reverse string with for loop
+  for (let j = s.length - 1; j >= 0; j--) {
+    sReversed += s[j];
+  }
+
+  // compare reversed string with original
+  return sReversed === s;
 };
 
 // Test cases
