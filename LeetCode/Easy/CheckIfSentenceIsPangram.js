df64a5db17b4ecdc4b915a57ced31def6eca88ad
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-05-15 15:12:09 -0400

    Add more concise method

--- LeetCode/Easy/CheckIfSentenceIsPangram.js
+++ LeetCode/Easy/CheckIfSentenceIsPangram.js
@@ -8,9 +8,15 @@
  * Leetcode 1832
  */
 
+// Runtime: 68 ms, faster than 99.04% of JavaScript online submissions for Check if the Sentence Is Pangram.
+// Memory Usage: 40.2 MB, less than 25.48% of JavaScript online submissions for Check if the Sentence Is Pangram.
+var checkIfPangram = function (sentence) {
+  return new Set(sentence).size === 26;
+};
+
 // Runtime: 84 ms, faster than 45.74% of JavaScript online submissions for Check if the Sentence Is Pangram.
 // Memory Usage: 38.8 MB, less than 70.88% of JavaScript online submissions for Check if the Sentence Is Pangram.
-var checkIfPangram = function (sentence) {
+var checkIfPangram2 = function (sentence) {
   let letters = new Set();
   for (let i = 0; i < sentence.length; i++) {
     letters.add(sentence[i]);
