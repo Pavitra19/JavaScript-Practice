a0aa19dced68b5513fe5eb1d5d485c49852c1470
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-06-25 13:10:03 -0400

    Add second method to detect capital

--- LeetCode/Easy/DetectCapital.js
+++ LeetCode/Easy/DetectCapital.js
@@ -7,7 +7,18 @@
  * All letters in this word are not capitals, like "leetcode".
  * Only the first letter in this word is capital, like "Google".
  * Given a string word, return true if the usage of capitals in it is right.
+ *
+ * Leetcode 520
  */
+
+// Runtime: 76 ms, faster than 96.46% of JavaScript online submissions for Detect Capital.
+// Memory Usage: 40.3 MB, less than 12.63% of JavaScript online submissions for Detect Capital.
+var detectCapitalUse = function (word) {
+  return (
+    word === word.toUpperCase() ||
+    word === word[0] + word.slice(1).toLowerCase()
+  );
+};
 
 // Runtime: 84 ms, faster than 80.30% of JavaScript online submissions for Detect Capital.
 // Memory Usage: 40 MB, less than 52.53% of JavaScript online submissions for Detect Capital.
